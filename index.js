b459be30084ca423be79fb9c66867ad0bf8ab9e3
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2019-07-10 14:42:27 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extendSchema: require('./lib/extendSchema'),
+  response: require('./lib/response'),
+  decompress: require('./lib/decompress'),
+  serializeOfficeXmls: require('./lib/serializeOfficeXmls')
+}
