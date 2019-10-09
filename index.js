ffe8c677fc044fc194628c354bd4fd8dcf9e0e23
BJR Matos <bjrmatos@gmail.com>
2019-10-09 13:09:37 -0500

    Revert "Revert "expose some helpers to detect supported office documents and add some properties to res.meta when in preview mode to help extra original office doc" for new release"
    This reverts commit 54a27cab1a8b9bc366d7caa710506a00c150a3fe.

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   extendSchema: require('./lib/extendSchema'),
   response: require('./lib/response'),
   decompress: require('./lib/decompress'),
+  officeDocuments: require('./lib/officeDocuments'),
   serializeOfficeXmls: require('./lib/serializeOfficeXmls')
 }
