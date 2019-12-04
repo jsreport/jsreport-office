e0df9519977aac2bb962d2c20a7ab8dc9c4a8496
BJR Matos <bjrmatos@gmail.com>
2019-12-04 17:29:47 -0500

    add new method saveXmlsToOfficeFile that it is does not depend on extra params

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   extendSchema: require('./lib/extendSchema'),
   response: require('./lib/response'),
   decompress: require('./lib/decompress'),
+  saveXmlsToOfficeFile: require('./lib/saveXmlsToOfficeFile'),
   serializeOfficeXmls: require('./lib/serializeOfficeXmls')
 }
